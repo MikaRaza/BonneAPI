e031fb0b1e74f0fe5215ecbd45a61b0fdba21afe
MikaRaza <107081961+MikaRaza@users.noreply.github.com>
2023-11-07 19:31:43 +0200

    router products

--- routers/router_products.py
+++ routers/router_products.py
@@ -6,7 +6,6 @@ from classes.schema_dto import products, productsNoID
 from database.firebase import db
 router = APIRouter(prefix='/products', tags=['products'])
 
-
 class Product(BaseModel):
     id: str
     name: str
@@ -24,42 +23,33 @@ products_list = [
 async def get_product():
     return products_list
 
-
 @router.post('/', response_model=products, status_code=201)
 async def create_product(givenName: productsNoID): 
     generatedId = str(uuid.uuid4())   
-    newproducts = products(id=generatedId, name=givenName.name)    
-    products.append(newproducts)
-    db.child("products").child(generatedId).set(newproducts.model_dump())  
-    return newproducts
-
+    new_product = Product(id=generatedId, name=givenName.name)    
+    products_list.append(new_product)
+    db.child("products").child(generatedId).set(new_product.model_dump())  
+    return new_product
 
 @router.get('/{products_id}', response_model=products)
 async def get_products_by_ID(products_id: str):
-    # On parcourt chaque étudiant de la liste
-    for products in products:
-        # Si l'ID correspond, on retourne l'étudiant trouvé
-        if products.id == products_id:
-            return products
-    # Si on arrive ici, c'est que la boucle sur la liste "products" n'a rien trouvé
-    # On lève donc un HTTP Exception
-    raise HTTPException(status_code=404, detail="products not found")
-
+    for product in products_list:
+        if product.id == products_id:
+            return product
+    raise HTTPException(status_code=404, detail="Product not found")
 
 @router.patch('/{products_id}', status_code=204)
-async def modify_products_name(products_id: str, modifiedproducts: productsNoID):
-    for products in products:
-        if products.id == products_id:
-            products.name = modifiedproducts.name
+async def modify_products_name(products_id: str, modified_product: productsNoID):
+    for product in products_list:
+        if product.id == products_id:
+            product.name = modified_product.name
             return
-    raise HTTPException(status_code=404, detail="products not found")
-
+    raise HTTPException(status_code=404, detail="Product not found")
 
 @router.delete('/{products_id}', status_code=204)
-async def delete_products(products_id: str):
-    for products in products:
-        if products.id == products_id:
-            products.remove(products)
+async def delete_product(products_id: str):
+    for product in products_list:
+        if product.id == products_id:
+            products_list.remove(product)
             return
-    raise HTTPException(status_code=404, detail="products not found")
-
+    raise HTTPException(status_code=404, detail="Product not found")
