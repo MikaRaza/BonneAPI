932c083aec34c89f443cb94c649c4c4bbde8b10f
MikaRaza <107081961+MikaRaza@users.noreply.github.com>
2023-11-08 16:15:11 +0200

    Update description.py

--- documentation/description.py
+++ documentation/description.py
@@ -1,9 +1,10 @@
 api_description = """
 # Pour trouver votre API-ritif
 
-Utilisateurs, lien vers une ressource
+Propriétaire d'une petite épicerie ou grande, la taille ne compte pas, vous trouverez votre bonheur ici ! et surtout Bon apétit (et non bonne degustation)
 
-API vendu à des centre de ventes de nourittures (Supermarchés, Episiri) ... "Center" -> Sessions + products -> Attendances
-
+API vendu à des centre de ventes de nourittures (Supermarchés, Episiri) ... "Center" ->  products 
+[CRUD]
+[AUTH]
 [checkout](/Stripe/checkout)
 """
